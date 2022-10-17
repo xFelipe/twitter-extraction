b87b6b3f1cb4b748f22070082a269cc9b259ee8a
Felipe Gomes Duarte <felipe.duarte@cortex-intelligence.com>
2022-10-16 23:05:51 -0300

    paginação

--- teste.py
+++ teste.py
@@ -1,24 +1,70 @@
-import json 
+import json
 import os
-from requests import get
+from requests import get, Response
 from dotenv import load_dotenv
+from datetime import datetime, timedelta, date
+from logging import Logger, INFO
 
+logger = Logger(name=__name__, level=INFO)
+logger.setLevel(INFO)
 load_dotenv()
 
-auth_token = os.environ["AUTH_TOKEN"]
-header = {"Authorization": "Bearer "+auth_token}
-params = {
-    "query": "Lula",
-    "tweet.fields": "author_id,text,geo,id,in_reply_to_user_id,lang",
-    "expansions": "author_id,referenced_tweets.id,referenced_tweets.id.author_id,in_reply_to_user_id",
-    "start_time": "2022-10-05T00:00:00z",
-    "end_time": "2022-10-07T23:59:59z",
-    "max_results": 10
-}  # non_public_metrics, public_metrics, organic_metrics, promoted_metrics, possibly_sensitive, referenced_tweets, source, withheld)
 
-res = get("https://api.twitter.com/2/tweets/search/recent", params=params, headers=header)
-print(res)
-response_json = json.dumps(res.json())
+def __validate_response(response: Response):
+    if response.status_code != 200:
+        logger.error(response.json().get("errors"))
+    else:
+        logger.info(response)
+    response.raise_for_status()
 
-with open("result.json", "w") as f:
-    f.write(response_json)
+
+def __get_header():
+    auth_token = os.environ["AUTH_TOKEN"]
+    return {"Authorization": "Bearer "+auth_token}
+
+
+def __paginate(next_token: str, query: str):
+    url = f"https://api.twitter.com/2/tweets/search/recent?query={query}&next_token={next_token}"
+    
+    res = get(url, headers=__get_header())
+    __validate_response(res)
+
+    response_json = json.dumps(
+        res.json(), ensure_ascii=False).encode('utf-8').decode()
+
+    yield response_json
+
+    next_token = res.json().get("meta", {}).get("next_token", "")
+    if (next_token):
+        yield from __paginate(next_token, query)
+
+
+def search_tweets(query: str, dt: date = None):
+    search_date = dt or (datetime.now() - timedelta(days=1))
+    params = {
+        "query": query,
+        "tweet.fields": "author_id,text,geo,id,in_reply_to_user_id,lang",
+        "expansions": "author_id,referenced_tweets.id,referenced_tweets.id.author_id,in_reply_to_user_id",
+        "start_time": f"{search_date.year}-{search_date.month}-{search_date.day}T00:00:00z",
+        "end_time": f"{search_date.year}-{search_date.month}-{search_date.day}T23:59:59z",
+        "max_results": 10
+    }  # non_public_metrics, public_metrics, organic_metrics, promoted_metrics, possibly_sensitive, referenced_tweets, source, withheld)
+
+    res = get("https://api.twitter.com/2/tweets/search/recent",
+              params=params, headers=__get_header())
+    __validate_response(res)
+
+    response_json = json.dumps(
+        res.json(), ensure_ascii=False).encode('utf-8').decode()
+
+    yield response_json
+
+    next_token = res.json().get("meta", {}).get("next_token", "")
+    if (next_token):
+        yield from __paginate(next_token, query)
+
+
+if __name__ == "__main__":
+    with open("result.json", "w") as f:
+        for result in search_tweets("Lula"):
+            f.write(result)
